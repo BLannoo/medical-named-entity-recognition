50e537bb0ee84f58e49315027ac152072b2196eb
Bruno Lannoo <Bruno.Lannoo@nike.com>
2021-07-20 15:44:06 +0200

    Preprocess pubtator data to csv

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,117 +1,79 @@
 import json
-from dataclasses import dataclass, asdict
 from glob import glob
+from json import JSONDecodeError
 from pathlib import Path
-from typing import List, Optional
+from typing import List
 
 import pandas as pd
 import plac
 import spacy
-from spacy.tokens.token import Token
 
 from src.definitions import PROJECT_ROOT
-from src.logger import logger
 
 
-def preprocess_data(project_root: Path = PROJECT_ROOT):
-    df_tokens_with_features = pd.DataFrame(
-        asdict(engineer_features(token_with_context))
-        for pubtator_json in list_all_pubtator_jsons(project_root)
-        for annotated_passage in extract_annotated_passages(pubtator_json)
-        for token_with_context in generate_all_annotated_tokens(annotated_passage)
-    )
-    persist_tokens_with_features(df_tokens_with_features, project_root)
+def preprocess_data(
+    data_root: Path = PROJECT_ROOT / "data/raw",
+    output_dir: Path = PROJECT_ROOT / "data/processed",
+) -> None:
+    examples = read_all_pubtator_examples(data_root)
 
-
-def list_all_pubtator_jsons(project_root: Path) -> List[dict]:
-    return [
-        json.loads(Path(path).open().read())
-        for path in glob(str(project_root / "data/raw/*/*.json"))
-    ]
-
-
-@dataclass(frozen=True)
-class Annotation:
-    offset: int
-    length: int
-    type: str
-
-
-@dataclass(frozen=True)
-class AnnotatedPassage:
-    text: str
-    annotations: List[Annotation]
-
-
-def extract_annotated_passages(pubtator_json: dict) -> List[AnnotatedPassage]:
-    # TODO: handle multiple locations for a single annotation
-    for passage in pubtator_json["passages"]:
-        for annotation in passage["annotations"]:
-            if len(annotation["locations"]) != 1:
-                logger.warn(
-                    f"multiple locations not supported for now, example: {pubtator_json}"
-                )
-                return []
-
-    return [
-        AnnotatedPassage(
-            text=passage["text"],
-            annotations=[
-                Annotation(
-                    offset=annotation["locations"][0]["offset"],
-                    length=annotation["locations"][0]["length"],
-                    type=annotation["infons"]["type"],
-                )
-                for annotation in passage["annotations"]
-            ],
-        )
-        for passage in pubtator_json["passages"]
-        if passage["text"] != ""
-    ]
-
-
-@dataclass(frozen=True)
-class AnnotatedToken:
-    token: Token
-    ner_type: Optional[str]
-
-    @staticmethod
-    def create(token: Token, annotated_passage: AnnotatedPassage) -> "AnnotatedToken":
-        ner_types = [
-            annotation.type
-            for annotation in annotated_passage.annotations
-            if annotation.offset == token.idx
-        ]
-        return AnnotatedToken(
-            token=token,
-            ner_type=ner_types[0] if ner_types else None,
-        )
-
-
-def generate_all_annotated_tokens(
-    annotated_passage: AnnotatedPassage,
-) -> List[AnnotatedToken]:
     nlp = spacy.load("en_core_web_sm")
-    return [
-        AnnotatedToken.create(token, annotated_passage)
-        for token in nlp(annotated_passage.text)
+
+    df = pd.concat(
+        [
+            parse_passage(passage, example["id"], idx, nlp)
+            for example in examples
+            for idx, passage in enumerate(example["passages"])
+            if passage["text"] != ""
+        ]
+    )
+
+    persist(df, output_dir)
+
+
+def read_all_pubtator_examples(data_root: Path) -> List[dict]:
+    examples = [
+        parse_pubtator_file(path)
+        for path in glob(str(data_root / "**/*.json"), recursive=True)
     ]
+    return examples
 
 
-@dataclass(frozen=True)
-class TokenWithFeatures:
-    token: str
+def parse_pubtator_file(path: Path) -> dict:
+    try:
+        return json.loads(Path(path).open().read())
+    except JSONDecodeError as e:
+        raise Exception(f"Failed to parse the pubtator json at: {path=}") from e
 
 
-def engineer_features(annotated_passage: AnnotatedToken) -> TokenWithFeatures:
-    pass
+def parse_passage(
+    passage: dict, pubtator_id: str, passage_id: int, nlp
+) -> pd.DataFrame:
+    return pd.DataFrame(
+        [
+            {
+                "pubtator_id": pubtator_id,
+                "passage_id": passage_id,
+                "words": token.text,
+                "labels": [
+                    annotation["infons"]["type"]
+                    for annotation in passage["annotations"]
+                    for location in annotation["locations"]
+                    if location["offset"] == token.idx
+                ],
+            }
+            for token in nlp(passage["text"])
+        ]
+    )
 
 
-def persist_tokens_with_features(df: pd.DataFrame, project_root: Path) -> None:
-    (project_root / "data").mkdir(exist_ok=True)
-    (project_root / "data/processed").mkdir(exist_ok=True)
-    df.to_csv(project_root / "data/processed/token_with_features.csv", index=False)
+def persist(df: pd.DataFrame, output_dir: Path) -> None:
+    output_dir.mkdir(exist_ok=True)
+    df.to_csv(
+        output_dir / "labeled_passages.csv",
+        index=False,
+    )
 
 
 if __name__ == "__main__":
-    plac.call(engineer_features)
+    plac.call(preprocess_data)
--- src/test_preprocess_data.py
+++ src/test_preprocess_data.py
@@ -1,94 +1,18 @@
-import json
-import shutil
-from typing import List, Optional
+from pathlib import Path
 
 import pandas as pd
-import pytest
-from assertpy import assert_that
-from pandas._testing import assert_frame_equal
 
 from src.definitions import PROJECT_ROOT
-from src.preprocess_data import (
-    list_all_pubtator_jsons,
-    extract_annotated_passages,
-    Annotation,
-    AnnotatedPassage,
-    generate_all_annotated_tokens,
-    preprocess_data,
-)
+from src.preprocess_data import preprocess_data
 
 
-@pytest.fixture(autouse=True)
-def setup_raw_data_on_tmp_path(tmp_path):
-    (tmp_path / "data").mkdir()
-    (tmp_path / "data/raw").mkdir()
-    (tmp_path / "data/raw/0to99").mkdir()
-    shutil.copy(
-        PROJECT_ROOT / "data/test/pubtator_example_1.json",
-        tmp_path / "data/raw/0to99/1.json",
+def test_preprocess_data(tmp_path: Path):
+    preprocess_data(
+        data_root=PROJECT_ROOT / "data/test/raw",
+        output_dir=tmp_path,
     )
 
-
-def test_engineer_features(tmp_path):
-    preprocess_data(project_root=tmp_path)
-
-    assert_frame_equal(
-        left=pd.read_csv(PROJECT_ROOT / "data/processed/token_with_features.csv"),
-        right=pd.DataFrame(
-            [
-                {"token": "Formate"},
-                {"token": "assay"},
-                {"token": "in"},
-                {"token": "body"},
-                {"token": "fluids"},
-                {"token": "application"},
-                {"token": "in"},
-                {"token": "methanol"},
-                {"token": "poisoning"},
-            ]
-        ),
+    pd.testing.assert_frame_equal(
+        left=pd.read_csv(tmp_path / "labeled_passages.csv"),
+        right=pd.read_csv(PROJECT_ROOT / "data/test/expected_labeled_passages.csv"),
     )
-
-
-def test_list_all_pubtator_jsons(tmp_path):
-    all_pubtator_jsons = list_all_pubtator_jsons(tmp_path)
-    assert_that(all_pubtator_jsons).is_length(1)
-    assert_that(all_pubtator_jsons[0]).is_type_of(dict)
-    assert_that(all_pubtator_jsons[0]["passages"]).extracting("text").contains(
-        "Formate assay in body fluids: application in methanol poisoning."
-    )
-
-
-EXAMPLE_ANNOTATED_PASSAGE = AnnotatedPassage(
-    text="Formate assay in body fluids: application in methanol poisoning.",
-    annotations=[
-        Annotation(offset=45, length=8, type="Chemical"),
-        Annotation(offset=54, length=9, type="Disease"),
-    ],
-)
-
-
-def test_extract_annotated_passages():
-    annotated_passages = extract_annotated_passages(
-        json.loads((PROJECT_ROOT / "data/test/pubtator_example_1.json").open().read())
-    )
-    assert_that(annotated_passages).is_length(1)
-    assert_that(annotated_passages[0]).is_equal_to(EXAMPLE_ANNOTATED_PASSAGE)
-
-
-def test_generate_all_tokens_with_context():
-    all_annotated_tokens = generate_all_annotated_tokens(EXAMPLE_ANNOTATED_PASSAGE)
-
-    def check_tokens_by_ner_type(ner_type: Optional[str], tokens: List[str]) -> None:
-        assert_that(all_annotated_tokens).extracting(
-            "token", filter=lambda x: x.ner_type is ner_type
-        ).extracting("text").contains(*tokens)
-
-    check_tokens_by_ner_type(
-        ner_type=None,
-        tokens=["Formate", "assay", "in", "body", "fluids", "application", "in"],
-    )
-
-    check_tokens_by_ner_type(ner_type="Chemical", tokens=["methanol"])
-
-    check_tokens_by_ner_type(ner_type="Disease", tokens=["poisoning"])
